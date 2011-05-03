aed842162bb6412078833993185a483f1bc6b9bb
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2011-05-03 14:43:42 -0700

    hello git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TableTennis::Application.config.secret_token = 'c131b2fc37887c332b7c105c1b34c34104fd570c91bdb8a787e5274a93621504f99eab1bcd7782f0d98fe68ed01b12d7b305ed5a2ac4bbe2b8090bbad4e56933'
