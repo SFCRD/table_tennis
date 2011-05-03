848ced3d0c39c748affc6a17858302d4f3544d21
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2011-05-03 15:17:51 -0700

    add some db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+League.create( :name => 'AKQA Table Tennis' ).tap do |league|
+  
+  [ 'James De Jesus', 'Vinit Patil', 'Thomas Ko', 'Miles Lukas' ].each do |name|
+    
+    Player.create( :name => name ).tap do |player|
+      
+      Ranking.create( :player => player, :league => league )
+      
+    end
+    
+  end
+  
+end
