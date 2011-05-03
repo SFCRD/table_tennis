61a8a7f2d5124d47f662dc618e0e88d54026045e
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2011-05-03 14:50:41 -0700

    add validation for player name

--- test/unit/player_test.rb
+++ test/unit/player_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  test "should not save user without a name" do
+    player = Player.new
+    assert_equal false, player.save
+  end
+  test "should validate the presence of a name" do
+    player = Player.new
+    player.save
+    assert_error_on :name, player
   end
 end
