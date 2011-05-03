7cd65f825dceda18edcc6a41bd2788ebcd738bad
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2011-05-03 14:52:53 -0700

    add league model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110503214518) do
+ActiveRecord::Schema.define(:version => 20110503215059) do
+
+  create_table "leagues", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "players", :force => true do |t|
     t.string   "name"
---
+++ test/fabricators/league_fabricator.rb
@@ -0,0 +1,5 @@
+Fabricator( :league ) do
+  
+  name { Faker::Lorem.words 3 }
+  
+end
